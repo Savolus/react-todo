790be5d5e72299ac47d6d8ee27aa1fb63c90220e
Mykola Dorohyi <n.dorogoy@gmail.com>
2020-11-29 23:10:33 +0200

    counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import { useState } from 'react'
 import './App.css';
 
 function App() {
-	return <div/>
+	const [counter, setCounter] = useState(0)
+
+	return (
+		<div>
+			{counter}
+			<button onClick={() => setCounter(counter + 1)}>
+				+1
+			</button>
+		</div>
+	)
 }
 
 export default App;
