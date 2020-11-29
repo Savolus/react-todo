74349459ff36627b7c754a12f185370798247b76
Mykola Dorohyi <n.dorogoy@gmail.com>
2020-11-30 00:49:07 +0200

    counter

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ function App() {
 	const [counter, setCounter] = useState(0)
 
 	return (
-		<div>
-			{counter}
-			<button onClick={() => setCounter(counter + 1)}>
+		<div className="box">
+			<div className="counter">{counter}</div>
+			<button className="decrement" onClick={() => setCounter(counter - 1)}>
+				-1
+			</button>
+			<button className="increment" onClick={() => setCounter(counter + 1)}>
 				+1
 			</button>
 		</div>
